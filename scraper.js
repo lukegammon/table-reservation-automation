f13d472817bb58d2180a7b1b751feb91424293b3
Luke Gammon <rustinpiece@gmail.com>
2021-07-26 17:35:12 +0100

    headless to true

--- scraper.js
+++ scraper.js
@@ -2,12 +2,11 @@ const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
 require('dotenv').config()
 async function getAvailability() {
-  const date = "2021-08-09"; // YYYY-MM-DD
+  const date = "2021-08-08"; // YYYY-MM-DD
   const hour = "15"; // Hour table is needed
   const URL = `https://www.opentable.co.uk/r/the-ivy-exeter?corrid=812656ee-0181-4506-8e7c-1c67b55eb207&avt=eyJ2IjoyLCJtIjowLCJwIjowLCJzIjowLCJuIjowfQ&p=2&sd=${date}T${hour}%3A00%3A00`
-  const browser = await puppeteer.launch({ headless: false }); // For test disable the headless mode,
+  const browser = await puppeteer.launch({ headless: true }); // For test disable the headless mode,
   const page = await browser.newPage();
-  await page.setViewport({ width: 1000, height: 926 });
   await page.goto(URL, { waitUntil: 'networkidle2' });
   console.log("Searching for table availability...");
   const services = await page.evaluate(() =>
@@ -55,7 +54,7 @@ function sendEmail(result) {
 }
 
 // Run getAvailibility() every X milliseconds to auto search until booking timeslot is found
-const searchInterval = 1800000;
+const searchInterval = 10000//1800000;
 const repeater = setInterval(async () => {
   const isTable = await getAvailability();
   if(isTable) {
